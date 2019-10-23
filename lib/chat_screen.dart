aa8c494d855554bc6537e655c593e866264e4bde
GabrielM <gabriel.martinez@inalambra.com>
2019-10-23 14:24:55 -0500

    laboratorie finished

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,21 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'chat_message.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _textController =
-      new TextEditingController(); //new
-
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Friendlychat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
       ),
-      body: _buildTextComposer(),
+      body: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (BuildContext _, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              Divider(),
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border: new Border(
+                    top: new BorderSide(color: Colors.grey[200]),
+                  ),
+                )
+              : null),
     );
   }
 
@@ -27,29 +57,35 @@ class ChatScreenState extends State<ChatScreen> {
         child: Row(
           children: <Widget>[
             Flexible(
-              fit: FlexFit.tight,
-              child: FlatButton(
-                child: Text("D"),
-                onPressed: () {},
-                color: Colors.red,
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message",
+                ),
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
               ),
-              // TextField(
-              //   controller: _textController,
-              //   onSubmitted: _handleSubmitted,
-              //   decoration: new InputDecoration.collapsed(
-              //     hintText: "Send a message",
-              //   ),
-              // ),
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: IconButton(
-                icon: Icon(
-                  Icons.send,
-                ),
-                onPressed: () {},
-              ),
-            )
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? new CupertinoButton(
+                      child: new Text("Send"),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    )
+                  : new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    ),
+            ),
           ],
         ),
       ),
@@ -58,5 +94,28 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        //new
+        duration: new Duration(milliseconds: 700), //new
+        vsync: this, //new
+      ),
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    //new
+    for (ChatMessage message in _messages) //new
+      message.animationController.dispose(); //new
+    super.dispose(); //new
   }
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,12 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:friendlychat/chat_screen.dart';
 
 class FriendlychatApp extends StatelessWidget {
+  final ThemeData kIOSTheme = new ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light,
+  );
+
+  final ThemeData kDefaultTheme = new ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400],
+  );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Friendlychat',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: ChatScreen(),
     );
   }
